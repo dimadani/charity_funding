28d4e81239c13dc2ffa8483d95784a9b77009862
zaken <nash510@gmail.com>
2020-08-12 21:42:43 +0700

    test

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,27 @@
-<h1>kasih air ke afrika yuk...</h1>
+<script>
+    import CharityList from './CharityList.svelte';
+
+    let title = "Charity";
+    setTimeout(() => {
+        title = "Donation";
+    }, 2000);
+
+    let charities = [];
+</script>
+
+<!-- <svelte:head>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.3.css" />
+</svelte:head> -->
+
+<div>
+    <h1>{title}</h1>
+    <CharityList {charities} />
+</div>
+
+<style>
+    :global(div) {
+        border: 5px solid black;
+        padding: 10px;
+    }
+</style>
+
---
+++ src/CharityList.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let charities = ['none'];
+</script>
+
+<div>
+    <h1>daftar charity</h1>
+    <pre>{charities}</pre>
+    <ul>
+        <li>carity 1</li>
+        <li>carity 2</li>
+        <li>carity 3</li>
+    </ul>
+</div>
